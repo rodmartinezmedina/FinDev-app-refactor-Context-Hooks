07a268567c21bb6b44c6128e9dceb8885fc4a0dc
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-05-19 17:33:46 +0200

    netlify.toml

--- src/contexts/github/GithubState.js
+++ src/contexts/github/GithubState.js
@@ -63,8 +63,8 @@ const GithubState = (props) => {
 
     const res = await axios.get(
       `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=
-    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-    ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    ${githubClientId}&client_secret=
+    ${githubClientSecret}`
     );
 
     dispatch({
