17a45870732ee3617f31f796715d3b58b1a5e211
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-10-31 14:33:53 +0100

    before migration

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,10 @@
 import React, { Fragment } from "react";
 import Search from "../users/Search";
 import Users from "../users/Users";
-import HomeHero from "../layout/HomeHero";
 
 const Home = () => {
   return (
     <Fragment>
-      {/* <HomeHero /> */}
       <Search />
       <Users />
     </Fragment>
